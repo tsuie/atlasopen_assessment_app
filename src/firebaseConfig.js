cdbd9c379c8680d921eedc1ac2281bc6b710eb81
JuddAO19 <juddguerrero@gmail.com>
2020-09-17 20:03:08 +1000

    Remove firebaseConfig.js values

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyD7KhDYomFFrQE4dHH6DDs-SNK9rfptZ9o",
-    authDomain: "assessment-app-41d0b.firebaseapp.com",
-    databaseURL: "https://assessment-app-41d0b.firebaseio.com",
-    projectId: "assessment-app-41d0b",
-    storageBucket: "assessment-app-41d0b.appspot.com",
-    messagingSenderId: "418813227371",
-    appId: "1:418813227371:web:22828bd50e32150598dc2e"
+    //TODO Replace with own config file
+    // apiKey: "AIzaSyD7KhDYomFFrQE4dHH6DDs-SNK9rfptZ9o",
+    // authDomain: "assessment-app-41d0b.firebaseapp.com",
+    // databaseURL: "https://assessment-app-41d0b.firebaseio.com",
+    // projectId: "assessment-app-41d0b",
+    // storageBucket: "assessment-app-41d0b.appspot.com",
+    // messagingSenderId: "418813227371",
+    // appId: "1:418813227371:web:22828bd50e32150598dc2e"
 };
 
 export default firebaseConfig;
