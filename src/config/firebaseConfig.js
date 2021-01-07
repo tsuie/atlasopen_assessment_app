eaa40f1b52ad29bbbc83f0237b34ce300722e3e0
Tsuie <alvinucsk@gmail.com>
2021-01-07 20:36:10 +0800

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,20 +1,13 @@
 var firebaseConfig = {
     //TODO Replace with own config file
-    // apiKey: "AIzaSyCIziiUtZxEOaYvPDMa86ncOpp6EG5azRE",
-    // authDomain: "alvin-db-project.firebaseapp.com",
-    // projectId: "alvin-db-project",
-    // storageBucket: "alvin-db-project.appspot.com",
-    // messagingSenderId: "976887684661",
-    // appId: "1:976887684661:web:9dd0531254d66e33901b7c",
-    // measurementId: "G-WMF6X6TV4M"
 
-    apiKey: "AIzaSyDSINGmBE3TuhB-k77SxKpzS1J6CgqpAaQ",
-    authDomain: "assessment-app-3.firebaseapp.com",
-    databaseURL: "https://assessment-app-3-default-rtdb.firebaseio.com",
-    projectId: "assessment-app-3",
-    storageBucket: "assessment-app-3.appspot.com",
-    messagingSenderId: "96766123206",
-    appId: "1:96766123206:web:1f1a0d5810da962c7ecbc7"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 
